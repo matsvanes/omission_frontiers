358b9ba8eabda1163ce3d8a6d0c196a44c6e9eaa
Mats van Es <m.vanes@donders.ru.nl>
2020-02-28 15:44:24 +0100

    add missing bracket.

--- datainfo.m
+++ datainfo.m
@@ -1,9 +1,9 @@
 % this script contains meta deta like file directories
 
-home_dir = '/project_qnap/3010000.02/reproducescript/andersen/';
+home_dir = '/project_qnap/3010000.02/reproducescript/omission_frontiers/';
 analysis_dir = [home_dir, 'analysis/'];
 reproduce_dir = [home_dir, 'reproduce/'];
 data_dir = [home_dir, 'data/'];
-script_dir = [home_dir, 'scripts/';
+script_dir = [home_dir, 'scripts/'];
 figures_dir = analysis_dir;
 fieldtrip_dir = [script_dir, 'fieldtrip/'];

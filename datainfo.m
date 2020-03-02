d041fad3e9f390f0312d23c66fbbe65727718839
Mats <m.vanes@donders.ru.nl>
2020-03-02 12:41:01 +0100

    Delete datainfo.m

--- datainfo.m
+++
@@ -1,9 +0,0 @@
-% this script contains meta deta like file directories
-
-home_dir = '/project_qnap/3010000.02/reproducescript/omission_frontiers/';
-analysis_dir = [home_dir, 'analysis/'];
-reproduce_dir = [home_dir, 'reproduce/'];
-data_dir = [home_dir, 'data/'];
-script_dir = [home_dir, 'scripts/'];
-figures_dir = analysis_dir;
-fieldtrip_dir = [script_dir, 'fieldtrip/'];

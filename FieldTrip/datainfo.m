ef959cacc7778081897dc786639fad0bda21d6c5
Mats van Es <m.vanes@donders.ru.nl>
2020-03-10 17:18:18 +0100

    change directories.

--- FieldTrip/datainfo.m
+++ FieldTrip/datainfo.m
@@ -5,5 +5,6 @@ analysis_dir = [home_dir, 'analysis/'];
 reproduce_dir = [home_dir, 'reproduce/'];
 data_dir = [home_dir, 'data/'];
 script_dir = [home_dir, 'omission_frontiers/FieldTrip/'];
-figures_dir = analysis_dir;
+figures_dir = [analysis_dir, 'figures/'];
+matlab_dir = home_dir;
 fieldtrip_dir = [home_dir, 'fieldtrip/'];

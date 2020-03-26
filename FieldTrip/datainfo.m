72757e7e58b6715726c335240b154c989503c08f
Mats van Es <m.vanes@donders.ru.nl>
2020-03-26 10:08:56 +0100

    change info.

--- FieldTrip/datainfo.m
+++ FieldTrip/datainfo.m
@@ -1,8 +1,8 @@
 % this script contains meta deta like file directories
 
+all_subjects = [1 2];
 home_dir = '/project_qnap/3010000.02/reproducescript/andersen/';
 analysis_dir = [home_dir, 'analysis/'];
-reproduce_dir = [home_dir, 'reproduce/'];
 data_dir = [home_dir, 'data/'];
 script_dir = [home_dir, 'omission_frontiers/FieldTrip/'];
 figures_dir = [analysis_dir, 'figures/'];
